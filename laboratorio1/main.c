4aa0db5f23500387288d3081d2fd381c1623efc5
dbustos106 <dbustos@unal.edu.co>
2021-04-11 04:07:07 -0500

    fscanf() funcionando

--- laboratorio1/main.c
+++ laboratorio1/main.c
@@ -29,29 +29,28 @@ int getHash (int id) {
     return id;
 }
 
-FILE* cargarDatos(){
+int main(){
     FILE *file;
-    printf("Inicio programa");
-    file=fopen("bogota-cadastral-2020-1-All-HourlyAggregate.txt", "r");
+    printf("Inicio programa\n");
+    file=fopen("bogota-cadastral-2020-1-All-HourlyAggregate.txt", "rb");
     if(file==NULL){
         printf("Error leyendo archivo \n");
     }else{
-        printf("Exito");
+        printf("Exito\n");
     }
-    fclose(file);
-    return file;
-}
 
-int main(){
-    FILE* file = cargarDatos();
-    char primerDato;
-    fscanf(file, "%c", &primerDato);
-    printf(" d:  %c",primerDato);
-    char primerDato2;
-    fscanf(file, "%c", &primerDato2);
-    printf(" d:  %c",primerDato2);
+    char* file_content = malloc(9*sizeof(char));
+
+    int leidos = fscanf(file, "%[^,]", file_content);
+    printf("%d", leidos);
+    for(int i = 0; i < 9; i++){
+        printf("EL elemento es: %c\n", *(file_content + i));
+    }
+    
+
     struct hashTable hash1;
     hash1.getHash = getHash;
     printf("Resultado: %d",hash1.getHash(1160));
+    fclose(file);
     return 0;
 }
