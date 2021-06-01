44ed7907984be0434e8ac4f06a61d71515a03c8a
dbustos106 <dbustos@unal.edu.co>
2021-06-01 15:04:33 -0500

    Avance mallos handle

--- laboratorio2/backend/p2-server.c
+++ laboratorio2/backend/p2-server.c
@@ -16,15 +16,12 @@
  
 #define PORT 3535
 #define BACKLOG 32
-#define THREAD_SIZE 32
 
-pthread_t hilos[THREAD_SIZE];
 pthread_mutex_t mutex;
+ClientEstruct* clientes;
 
-FILE* openFile(FILE* file, char* dir, char* modo);
 int verificar(int desc, char* message);
 void* handle_conection(void* pclient);
-void* function(void* arg);
 
 int main(){
     int serverfd, clientfd, r, opt = 1;
@@ -33,10 +30,6 @@ int main(){
 
     // Se crean las variables de control para la sección crítica
     pthread_mutex_init(&mutex, NULL);
-
-    for(int i = 0; i < THREAD_SIZE; i++){
-        pthread_create(&hilos[i], NULL,(void *) function, NULL);
-    }
 
     verificar(serverfd = socket(AF_INET, SOCK_STREAM, 0), "\n-->Error en socket():");
     
@@ -52,6 +45,8 @@ int main(){
     
     verificar(r = listen(serverfd, BACKLOG), "\n-->Error en Listen(): ");
     
+    clientes = malloc(32*sizeof(ClientEstruct));
+
     while(true){
         verificar(clientfd = accept(serverfd, (struct sockaddr *)&client, &tamano), 
                 "\n-->Error en accept: ");
@@ -62,34 +57,19 @@ int main(){
         ClientEstruct* clientEstruct = malloc(sizeof(ClientEstruct));
         clientEstruct->clientfd = clientfd;
         clientEstruct->clientIP = dirIp;
-    
-        pthread_mutex_lock(&mutex);
-        enqueue(clientEstruct);
-        pthread_mutex_unlock(&mutex);       
+
+        //while(//dormir hasta que alguna de las pos se libere){
+         //   sleep(0.5);
+        //}
+
+        pthread_t t;
+        pthread_create(&t, NULL,(void *) handle_conection, clientEstruct);     
     }
     return 0;
 }
 
-void* function(void * arg){
-    while(true){
-        pthread_mutex_lock(&mutex);
-        ClientEstruct* clientfd = dequeue();
-        pthread_mutex_unlock(&mutex);
-        if(clientfd != NULL){
-            handle_conection(clientfd);
-        }
-    }
-}
-
-int verificar(int desc, char* message){
-    if(desc < 0){
-        perror(message);
-        exit(-1);
-    }
-    return desc;
-}
-
 void* handle_conection(void *pclient){
+    printf("entro al handle\n");
     ClientEstruct clientEstruct = *((ClientEstruct *) pclient);
     int clientfd = clientEstruct.clientfd;
     int sourceId = 0, dstId = 0, hod = 0;
@@ -98,29 +78,31 @@ void* handle_conection(void *pclient){
 
     //strcat(dirIp, *clientEstruct.clientIP);
     free(pclient);
-
+    printf("ENTRO AL CICLO\n");
     while(true){
         int r, tam = 0;
-        char buffer[60];
-
-        while((r = recv(clientfd, buffer, sizeof(buffer)-tam-1, 0)) > 0){
+        char* buffer = malloc(60*sizeof(char));
+        printf("Estoy al inicio del ciclo\n");
+        while((r = recv(clientfd, buffer, 60*sizeof(char)-tam, 0)) > 0){
             tam = tam + r;
-            if(tam >= 60-1 || buffer[tam-1] == '\n'){
+            if(tam >= 60-1 || *(buffer + tam-1)  == '\n'){
                 break;
             } 
         }
         verificar(r, "\n-->Error en recv(): ");
-        buffer[tam-1] = 0;
-        
+
         printf("Request: %s\n", buffer);
         fflush(stdout);
-
-        // Interpretar el mensaje recibido
+        free(buffer);
+        /*// Interpretar el mensaje recibido
         char* campo;
-        char* numero;
-        char* metodo = "PUT";
-        char* metodo2 = "GET";
-        char* metodo3 = "CLOSE";
+        int numero;
+        char* metodo = malloc(4*sizeof(char));
+        sprintf(metodo, "%s","PUT");
+        char* metodo2 = malloc(4*sizeof(char));
+        sprintf(metodo2, "%s","GET");
+        char* metodo3 = malloc(6*sizeof(char));
+        sprintf(metodo3, "%s","CLOSE");
 
         if((numero = strstr(buffer, metodo)) != NULL) {
             if(strstr(buffer, "sourceid") != NULL){
@@ -146,11 +128,11 @@ void* handle_conection(void *pclient){
             campo = "][busqueda:";
             double mean_time = busqueda(sourceId, dstId, hod);
             verificar(r = send(clientfd, &mean_time, 8, 0), "Error en el send");
-            //sprintf(numero, "%f", mean_time);
+            sprintf(numero, "%f", mean_time);
 
         }
-        printf("Num: %s\n", campo);
-        printf("Num: %s\n", numero);
+        printf("Campo: %s\n", campo);
+        printf("Número: %s\n", numero);
         // Escritura del archivo log
         //struct timespec currentTime; 
         //clock_gettime(CLOCK_REALTIME, &currentTime);
@@ -160,10 +142,12 @@ void* handle_conection(void *pclient){
         //fprintf(log, "%s", clientEstruct.clientIP);
         //fprintf(log, "%s", dirIp);
         //pthread_mutex_unlock(&mutex); 
-
+*/
         //free(numero);
         //fclose(log);
         //free(campo);
+        int respuesta = 200;
+        verificar(r = send(clientfd, &respuesta, 4, 0), "Error en el send");
     }
 
 
@@ -171,4 +155,12 @@ void* handle_conection(void *pclient){
     printf("Closing connection\n");
 
     return NULL;
+}
+
+int verificar(int desc, char* message){
+    if(desc < 0){
+        perror(message);
+        exit(-1);
+    }
+    return desc;
 }
